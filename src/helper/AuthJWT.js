323536f734728a036c81e3c8cbe9d09158e077d6
Muh Jajuni <muhamadjajuni1@gmail.com>
2023-11-10 14:21:02 +0700

    v2 connection fix

--- src/helper/AuthJWT.js
+++ src/helper/AuthJWT.js
@@ -7,17 +7,40 @@ const AuthJWT = async (req, res, next) => {
     const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) {
-      return res
-        .status(401)
-        .json({ status: 401, message: "login gagal, server butuh token" });
+      return res.status(401).json({
+        status: 401,
+        message: "Login gagal, server memerlukan token",
+      });
     }
-    let decode = await jwt.verify(token, process.env.API_KEY_PRIVATE);
-    console.log(decode);
-    req.payload = decode;
+
+    const decoded = await jwt.verify(token, process.env.API_KEY_PRIVATE);
+    console.log(decoded);
+
+    // Jika diperlukan, tambahkan pengecekan untuk data tambahan di sini
+    // Contoh: if (!decoded.userId) throw new Error('Invalid token');
+
+    req.payload = decoded;
     next();
   } catch (error) {
-    console.error("error", error);
-    return res.status(404).json({ status: 404, message: "Invalid token" });
+    console.error("Error:", error);
+
+    // Periksa jenis kesalahan untuk menentukan respons yang sesuai
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        status: 401,
+        message: "Invalid token. Harap login kembali.",
+      });
+    } else if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        status: 401,
+        message: "Token telah kadaluarsa. Harap login kembali.",
+      });
+    } else {
+      return res.status(500).json({
+        status: 500,
+        message: "Internal Server Error",
+      });
+    }
   }
 };
 
