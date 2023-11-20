9f96c02b1e415efe962571ce25c860559b4fe8af
Muhamad Jajuni <133248802+MuhamadJajuni@users.noreply.github.com>
2023-11-20 21:35:40 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,24 @@
-/* eslint-disable no-undef */
-require('dotenv').config();
-const { Pool } = require('pg');
+// /* eslint-disable no-undef */
+// require('dotenv').config();
+// const { Pool } = require('pg');
 
-// const db = new Pool({
-//   connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+// // const db = new Pool({
+// //   connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+// // });
+
+// // Tangani error pada koneksi pool
+// db.on('error', (err) => {
+//   console.error('Database pool error:', err.message);
 // });
 
-// Tangani error pada koneksi pool
-db.on('error', (err) => {
-  console.error('Database pool error:', err.message);
-});
+// // Coba melakukan koneksi ke database menggunakan async/await
+// (async () => {
+//   try {
+//     await db.connect();
+//     console.log('Terhubung ke database!');
+//   } catch (err) {
+//     console.error('Koneksi ke database gagal:', err.message);
+//   }
+// })();
 
-// Coba melakukan koneksi ke database menggunakan async/await
-(async () => {
-  try {
-    await db.connect();
-    console.log('Terhubung ke database!');
-  } catch (err) {
-    console.error('Koneksi ke database gagal:', err.message);
-  }
-})();
-
-module.exports = db;
+// module.exports = db;
